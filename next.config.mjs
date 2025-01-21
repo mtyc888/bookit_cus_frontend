fae9992962f713bc36ad8d917fc25ff72338b035
marvin tan <ymarvintan@gmail.com>
2025-01-21 15:41:34 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://13.229.116.85:3001/api/:path*'
+        }
+      ]
+    }
+  };
+  
+  export default nextConfig;
